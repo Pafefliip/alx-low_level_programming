77b70220b02ba00dd4541025e251e7c925c25fe6
pafefliip <padifelix@hotmail.com>
2023-03-17 18:16:02 -0700

    correted puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - this is the main function
- * 
- * Return: always 0
+ *
+ * Return - always 0
  */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
